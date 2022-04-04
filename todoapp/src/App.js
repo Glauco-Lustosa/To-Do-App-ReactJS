6e3e6c7675a85f6632c9b33ccb4cca85f83860d1
Glauco Lustosa <glauco.lustosa@icloud.com>
2022-04-04 12:15:21 -0300

    Files Setup
    Setup Project

---
+++ todoapp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>To Do APP!</h1>
+      </header>
+      <Form/>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todoapp/src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = (e) => {
+    return(
+        <div className="todo-container">
+        <ul className="todo-list"></ul>
+      </div>
+    );
+ };
+
+ export default TodoList;
