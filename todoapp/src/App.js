c007c441d1a7bf21b1a0d09e0c9d3c8bc219ffdd
Glauco Lustosa <glauco.lustosa@icloud.com>
2022-04-05 12:08:07 -0300

    Delete Button
    Delete button is working

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -16,7 +16,7 @@ function App() {
       todos={todos} 
       setTodos={setTodos} 
       setInputText={setInputText}/>
-      <TodoList todos={todos}/>
+      <TodoList setTodos={setTodos} todos={todos}/>
     </div>
   );
 }
--- todoapp/src/components/TodoList.js
+++ todoapp/src/components/TodoList.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Todo from './Todo';
-const TodoList = ({todos}) => {
+const TodoList = ({todos,setTodos}) => {
 
     return(
         <div className="todo-container">
         <ul className="todo-list">
             {todos.map(todo => (
-              <Todo key={todo.id} text={todo.text}></Todo>
+              <Todo 
+              key={todo.id} 
+              text={todo.text}
+              todos={todos}
+              todo={todo}
+              setTodos={setTodos}
+              ></Todo>
             ))}
 
         </ul>
